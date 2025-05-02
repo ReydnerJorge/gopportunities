ec682c97e4294584a78975b12d1faba2835fca96
Reydner96 <reydneroliveirajorge96@gmail.com>
2025-05-02 12:08:55 -0300

    refactor: error SQLite

--- config/sqlite.go
+++ config/sqlite.go
@@ -10,7 +10,7 @@ import (
 
 func InitializeSQLite() (*gorm.DB, error) {
 	logger := GetLogger("sqlite")
-	dbPath := "./db/main/db"
+	dbPath := "../db/main.db"
 	// Check if  the database file exist
 	_, err := os.Stat(dbPath)
 	if os.IsNotExist(err) {
